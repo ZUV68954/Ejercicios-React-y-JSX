7b6121b247a918521284828eec9a7dabe2a5c9bb
ZUV68954 <ZUV68954@educastur.es>
2025-01-10 19:47:49 +0100

    Margin top added.

--- Boostrap/ejercicio2/src/App.js
+++ Boostrap/ejercicio2/src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (<div className="App">
         <header className="App-header">
             <Container>
-                <Stack>
+                <Stack style={{marginTop: '5%'}}>
                     <Row>
                         <Col xs={6} md={6} lg={6}>
                             <Image src={"./Espaiderman.jpg"}
